3b9ff56725e3895fd093017578508d3d73356c71
Henrik Bach <HenrikBach1>
2025-03-21 08:50:19 +0100

    Redefine transeive() and show how to use it in loop()
    Reset to GH name
    
    Reset to GH name

--- Smart-Bike-Light/Network.cpp
+++ Smart-Bike-Light/Network.cpp
@@ -6,8 +6,12 @@
 HardwareSerial mySerial(UART); // Initialize UART1
 rn2xx3 myLora(mySerial);       // Initialize LoRa instance
 
+//Global wide variables for :
+String message = "";       // Received message
+
+//Module wide variables for :
 namespace {
-    String devEUI = "";        // Global variable for DevEUI
+    String devEUI = "";        // DevEUI/HWEUI
 }
 
 void initialize_LoRaWAN() {
@@ -40,8 +44,11 @@ void initialize_module_rn2483_LoRa() {
     Serial.println("DevEUI: " + devEUI);
     Serial.println("Firmware: " + myLora.sysver());
 
-    myLora.sendRawCommand("mac set deveui " + devEUI); // Set DevEUI explicitly
-    bool join_result = myLora.initOTAA("BE010000000007BA", "064D3897B8F4382A8C937BCA890435D3");
+    //myLora.sendRawCommand("mac set deveui " + devEUI); // Set DevEUI explicitly
+    //OTAA: initOTAA(String AppEUI, String AppKey);
+    const char* appEUI = "0000000000000000"; //HB:
+    const char* appKey = "5EA67FF029810B31D0805D4749AA682E"; //HB:
+    bool join_result = myLora.initOTAA(appEUI, appKey); //HB:
     while (!join_result) {
         Serial.println("Join failed. Retrying...");
         delay(60000);
@@ -50,9 +57,10 @@ void initialize_module_rn2483_LoRa() {
     Serial.println("Successfully joined!");
 }
 
-TX_RETURN_TYPE send(Module module, Status status, const char* data) {
-    myLora.sendRawCommand("mac set conf 1"); // Enable confirmed uplink
-
-    TX_RETURN_TYPE response = TX_SUCCESS; //TODO:HB: For now: myLora.tx(data);
-    //delay(1000); //TODO: HB: 1% duty cycle implemented in this way, for now or does the WAN implementation this for us?
+TX_RETURN_TYPE transeive(Module module, Status status, const char* data) {
+    //myLora.sendRawCommand("mac set conf 1"); // Enable confirmed uplink
+    TX_RETURN_TYPE response = myLora.tx(data);
+    message = myLora.getRx();
+    delay(2000); //TODO: HB: 1% duty cycle implemented in this way, for now. Or does the WAN implementation this for us?
+    return response;
 }
