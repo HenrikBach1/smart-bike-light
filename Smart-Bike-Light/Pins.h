319254c778903e917a2b610f6f3a6bef31e07c8f
emilbrogeDTU <135963775+emilbrogeDTU@users.noreply.github.com>
2025-04-24 12:06:55 +0200

    Update Pins.h
    Add the rest of the pins

--- Smart-Bike-Light/Pins.h
+++ Smart-Bike-Light/Pins.h
@@ -6,15 +6,19 @@
 #include <Arduino.h> // Include Arduino library for digitalWrite and pinMode
 
 // Pin definitions for ESP32 board
-#define BUTTON_1        2   // GPIO for Button 1
+#define BUTTON_1        0   // GPIO for Button 1
 #define BUTTON_2        4   // GPIO for Button 2
+#define INTERNAL_LED    2   //Internal LED
+#define PWR_LED_PIN     5   // PIN for controlling big led (PWM)
 #define ADXL345_INT1    15  // GPIO for ADXL345 Interrupt 1
 #define RX              18  // UART RX
 #define TX              19  // UART TX
 #define SDA             21  // I2C SDA pin
 #define SCL             22  // I2C SCL pin
 #define LORA_RESET      23  // GPIO for LoRa Reset
+#define PHOTORESISTOR_PIN 34 // GPIO input for photoresistor analog read
 #define BAT_ADC_INPUT   36  // GPIO for Battery ADC input
 #define LDR_ADC_INPUT   39  // GPIO for LDR ADC input
+
 
 #endif // PINS_H
