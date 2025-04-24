61ffed90b8fe444364d8f85615061fb1ef1dcc18
Djanko007 <viclyhne@gmail.com>
2025-04-24 10:38:17 +0200

    Update LedBrightnessAdjust.cpp

--- Smart-Bike-Light/LedBrightnessAdjust.cpp
+++ Smart-Bike-Light/LedBrightnessAdjust.cpp
@@ -6,17 +6,23 @@ const int pwmFreq = 5000; //
 const int pwmResolution = 8; // 8-bit → 0–255
 
 void adjustBrightness() {
-  int input = Serial.parseInt();
-  if (input >= 0 && input <= 100) {
-    int pwmValue = map(input, 0, 100, 0, 255);       // Convert to 0–255
-    ledcWrite(ledPin, 255 - pwmValue);  // Invert for PNP
-    Serial.print("Brightness set to: ");
-    Serial.print(input);
-    Serial.println("%");
-  } else {
-    Serial.println("Invalid input. Enter a number from 0 to 100.");
-  }
-    // Flush the rest of the line if any leftover junk
-  while (Serial.available()) Serial.read();
-}
+  input.toUpperCase(); //Make case insensitive
 
+  int abIndex = input.indexOf('AB'); //Find position in string where B occures, return -1 if not found
+  if (abIndex != -1 && abIndex + 2 < input.length()) { //Check if abIndex is 0 or above (position in string) and check if its longer than the two characters AB
+    String numberPart = input.subString(abIndex + 2); //Make a string of the numbers behind abIndex position
+    int value = numberPart.toInt(); //Turn the numbers in the string into integers
+
+    if (value >= 0 && value <= 100) {
+      int pwmValue = map(value, 0, 100, 0, 255);
+      ledcWrite(ledPin, 255 - pwmValue);
+      Serial.print("Brightness set to: ");
+      Serial.print(value);
+      Serial.println("%");
+    } else {
+      Serial.println("Number after B must be 0 to 100.");
+    }
+  } else {
+    Serial.println("Input must include 'B' followed by a number.");
+  }
+}
