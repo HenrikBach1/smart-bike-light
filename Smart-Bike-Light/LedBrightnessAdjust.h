52dae6867d21d78544fab4c1845c3ecabed14e5e
Djanko007 <viclyhne@gmail.com>
2025-04-24 09:41:00 +0200

    Update LedBrightnessAdjust.h

--- Smart-Bike-Light/LedBrightnessAdjust.h
+++ Smart-Bike-Light/LedBrightnessAdjust.h
@@ -1,20 +1,3 @@
-const int ledPin = 5; //Choose ledPin, has to be PWM compatable. Pin 5 on the ESP32 is
-const int pwmFreq = 5000; // 
-const int pwmResolution = 8; // 8-bit → 0–255
-
-void setup() {
-  Serial.begin(115200);
-  ledcAttach(ledPin, pwmFreq, pwmResolution); // Adjusted ESP32 package
-  Serial.println("Enter brightness (0-100):"); 
-}
-
-void loop() {
-  if (Serial.available() > 0) { //Check if serial is open
-  
-    adjustBrightness(); //Function that reads for 
-  }
-}
-
 void adjustBrightness() {
   int input = Serial.parseInt();
   if (input >= 0 && input <= 100) {
