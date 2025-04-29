67bf20ad550d12a7b0960dc9ef7d0325a497b18d
Henrik Bach <HenrikBach1>
2025-04-29 14:31:02 +0200

    Refactor WiFiScanner header and implementation to ensure proper handling of ENABLE_WIFI_SCANNER flag and add empty function stubs when disabled

--- Smart-Bike-Light/WiFiScanner.h
+++ Smart-Bike-Light/WiFiScanner.h
@@ -4,6 +4,10 @@
 #define WIFISCANNER_H
 
 #include <Arduino.h>   // ← pull in the core + event types
+#include "Globals.h"   // Include Globals.h for ENABLE_WIFI_SCANNER flag
+
+#if ENABLE_WIFI_SCANNER
+
 #include <WiFi.h>
 #include <string.h> 
 
@@ -11,4 +15,13 @@ void initWiFiScanner();
 void stopWiFiScanner();
 int getTop3Networks(uint8_t macs[3][6], uint8_t rssis[3]);
 
-#endif
+#else
+
+// Empty function stubs when module is disabled
+inline void initWiFiScanner() {}
+inline void stopWiFiScanner() {}
+inline int getTop3Networks(uint8_t macs[3][6], uint8_t rssis[3]) { return 0; }
+
+#endif // ENABLE_WIFI_SCANNER
+
+#endif // WIFISCANNER_H
