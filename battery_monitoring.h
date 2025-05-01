ec81f74f78a6e2139cc4eae681aea7f652ca4225
AsbjornSchroll <135966364+AsbjornSchroll@users.noreply.github.com>
2025-05-01 09:44:33 +0200

    Update battery_monitoring.h

--- battery_monitoring.h
+++ battery_monitoring.h
@@ -1,27 +1,33 @@
-#ifndef MYFUNCTIONS_H
-#define MYFUNCTIONS_H
+// battery_monitoring.h
+#ifndef BATTERY_MONITORING_H
+#define BATTERY_MONITORING_H
 
-const int resolution = 4096;
-const int adc_check_voltage = 15;
-const int adc_check_charging = 2;
-const int interval = 1000*60*10;
-const float max_adc_volt = 3.3f;
-const float battery_full_volt = 4.2f;
-bool charging = false;
-unsigned long last_read = 0;
-unsigned long current_read = 0;
-
+#include <Arduino.h>
 
 class battery_monitoring {
+public:
+    /// Konverterer rå ADC-værdi til batteriprocent (0–100)
+    int adc_voltage_to_bb_percent(int adc_voltage);
 
-  public:
-
-    int adc_voltage_to_bb_percent();
+    /// Returnerer true hvis enheden oplades lige nu
     bool is_charging();
+
+    /// Tjekker batterilevetid (eksempel-placeholder)
     int check_battery_lifetime();
 
+private:
+    // --- Konstanter ---
+    static constexpr int   resolution = 4096;    // ADC range
+    static constexpr int   adc_check_voltage_pin = 15;                    // pin til batteri-måling
+    static constexpr int   adc_check_charging_pin = 2;                     // pin til opladnings-detektion
+    static constexpr unsigned long interval = 1000UL * 60UL * 10UL; // 10 minutter
+    static constexpr float max_adc_volt = 3.3f;                  // referencespænding
+    static constexpr float battery_full_volt = 4.2f;                  // fuldt batteri
+
+    // --- Tilstand ---
+    bool charging = false;
+    unsigned long last_read = 0;
+    unsigned long current_read = 0;
 };
 
-#endif
-
-
+#endif // BATTERY_MONITORING_H
