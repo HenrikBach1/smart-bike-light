4b456ad98befba7e2245b6b5ec253cfb3711d765
AsbjornSchroll <135966364+AsbjornSchroll@users.noreply.github.com>
2025-05-01 13:49:45 +0200

    Update Pins.h

--- Pins.h
+++ Pins.h
@@ -10,6 +10,6 @@
 #define TX      19  // UART TX
 #define RST     23  // Reset pin
 #define UART    1   // UART1
-#define charge_pin 15 // Pin connected to IN+ on charging circuit. Used for battery monitoring
+#define charge_pin 15 // Pin connected to IN+ on charging circuit. (battery monitoring)
 #define adc_pin 4 // Pin connected to B+ on charging circuit. Used for battery monitoring (battery percentage/lifetime)
 #endif // PINS_H
