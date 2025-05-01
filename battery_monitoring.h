c87e04c5f59df61caba1a4ae1212fa95c38f7c3d
AsbjornSchroll <135966364+AsbjornSchroll@users.noreply.github.com>
2025-05-01 09:38:12 +0200

    Update battery_monitoring.h

--- battery_monitoring.h
+++ battery_monitoring.h
@@ -1,6 +1,16 @@
 #ifndef MYFUNCTIONS_H
 #define MYFUNCTIONS_H
 
+const int resolution = 4096;
+const int adc_check_voltage = 15;
+const int adc_check_charging = 2;
+const int interval = 1000*60*10;
+const float max_adc_volt = 3.3f;
+const float battery_full_volt = 4.2f;
+bool charging = false;
+unsigned long last_read = 0;
+unsigned long current_read = 0;
+
 
 class battery_monitoring {
 
